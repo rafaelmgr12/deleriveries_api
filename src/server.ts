996d47c5935c441f141913b83f2134241d2520d0
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-01-31 13:20:58 -0300

    Tratativas de erros

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
+
+import "express-async-errors";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(routes);
 
-app.listen(3000,()=>console.log("Server started on port 3000"));
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: "error",
+      message: "Internal server Error",
+    });
+  }
+);
 
+app.listen(3000, () => console.log("Server started on port 3000"));
